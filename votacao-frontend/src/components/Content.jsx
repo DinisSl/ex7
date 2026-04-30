ffec64c8367d52ede25d8f694652fdd7d373d0b4
DinisSL <dinissantanalopes@gmail.com>
2026-04-30 10:28:04 +0100

    react router 2

---
+++ votacao-frontend/src/components/Content.jsx
@@ -0,0 +1,17 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import QuestionTable from "./QuestionTable";
+import { Container, Row, Col } from "reactstrap";
+
+function Content() {
+  return (
+    <Container style={{ marginTop: "20px", maxWidth: "800px" }}>
+      <Row>
+        <Col>
+          <QuestionTable/>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default Content;

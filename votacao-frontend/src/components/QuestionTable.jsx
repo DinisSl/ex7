6268bbb98718d3afcc7d87bc747b0a28f439697a
DinisSL <dinissantanalopes@gmail.com>
2026-04-30 11:47:22 +0100

    Detail feito

--- votacao-frontend/src/components/QuestionTable.jsx
+++ votacao-frontend/src/components/QuestionTable.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { Table } from "reactstrap";
-import DetailModal from "./DetailModal";
-import VoteModal from "./VoteModal";
 import axios from "axios";
+
+import {useNavigate} from "react-router-dom";
 
 function QuestionTable() {
   const URL_QUESTIONS = "http://localhost:8000/votacao/api/questions/"; // (1)
@@ -21,26 +21,33 @@ function QuestionTable() {
 
   const centered = { textAlign: "center" };
 
+  const navigate = useNavigate();
+
   return (
     <Table light="true">
+
       <thead>
         <tr>
           <th>Texto</th>
           <th style={centered}>Controls</th>
         </tr>
       </thead>
+
       <tbody>
+
         {questionList.map((question) => (
           <tr key={question.id}>
             <td>{question.questao_texto}</td>
             <td style={centered}>
-              <DetailModal question={question}/>
+              <button onClick={() => navigate("/Details", {state:{id:question.id}})}>Detalhes</button>
               &nbsp;
-              <VoteModal question={question}/>
+              <button onClick={() => navigate("/Vote")}>Votar</button>
             </td>
           </tr>
         ))}
+
       </tbody>
+
     </Table>
   );
 }
--- votacao-frontend/src/pages/Details.jsx
+++ votacao-frontend/src/pages/Details.jsx
@@ -1,19 +1,67 @@
+import {useNavigate, useLocation} from "react-router-dom";
+import React, {useEffect, useState} from "react";
+import axios from "axios";
+import {Button, Form, FormGroup, Table} from "reactstrap";
+import moment from "moment";
 
 
 const Details = () => {
 
+  const location = useLocation();
+  const questaoRecebida = location.state.id;
 
-    return (
-        <>
+  const URL_OPTIONS = "http://localhost:8000/votacao/api/options/";
+  const URL_COMMENTS = "http://localhost:8000/votacao/api/comentarios/";
 
-            <main>
+  const [optionList, setOptionList] = useState([]);
+  const [commentList, setCommentList] = useState([]);
 
-            </main>
+  const navigate = useNavigate();
 
-        </>
-    )
+  useEffect(() => {
+    axios.get(URL_OPTIONS + questaoRecebida).then(request => {
+      setOptionList(request.data);
+    });
+    axios.get(URL_COMMENTS + questaoRecebida).then(request => {
+      setCommentList(request.data);
+    });
+  }, [questaoRecebida]);
 
+  return (
+    <>
+      <Form> {/* (3) */}
+        <FormGroup>
+          <b>Texto:</b>
+          <p>{questaoRecebida.questao_texto}</p>
+          <b>Data de publicação:</b>
+          <p>{moment(questaoRecebida.pub_data).format("YYYY-MM-DD HH:mm")}</p> {/* (4) */}
+        </FormGroup>
 
+        <FormGroup>
+          <Table>
+            <thead>
+              <tr>
+                <th style={{ textAlign: "left" }}>Opção</th>
+                <th style={{ textAlign: "right" }}>Votos</th>
+              </tr>
+            </thead>
+            <tbody>
+              {optionList.map((o) => (
+                // (5)
+                <tr key={o.id}>
+                  <td style={{ textAlign: "left" }}>{o.opcao_texto}</td>
+                  <td style={{ textAlign: "right" }}>{o.votos}</td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </FormGroup>
+    </Form>
+
+    <Button onClick={() => navigate("/")}>Voltar</Button>
+    </>
+  );
 }
+
 
 export default Details
